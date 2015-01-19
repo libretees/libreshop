4ec37c2def6118201e0bc2976b33036f98317247
Jared Contrascere <jcontra@gmail.com>
2015-01-19 04:34:34 +0000

    Create store.models.Product model.

---
+++ gossamer/store/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=200)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=12)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
