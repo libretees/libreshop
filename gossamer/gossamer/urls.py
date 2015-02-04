ca3ad4110e6e9afc86173a3e0f3f707f2247614f
Jared Contrascere <jcontra@gmail.com>
2015-02-04 16:31:21 +0000

    Remove store app.

--- gossamer/gossamer/urls.py
+++ gossamer/gossamer/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gossamer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
--- gossamer/store/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from store.models import Product
-
-# Register your models here.
-admin.site.register(Product)
