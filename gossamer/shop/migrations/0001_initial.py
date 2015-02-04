4933a048453358ea28a265ab267ade6c500a2655
Jared Contrascere <jcontra@gmail.com>
2015-02-04 21:39:04 +0000

    Refactor database migrations.

--- gossamer/shop/migrations/0001_initial.py
+++ gossamer/shop/migrations/0001_initial.py
@@ -2,23 +2,38 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 import jsonfield.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='Cart',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('saved_product', jsonfield.fields.JSONField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('customer', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Product',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
-                ('sku', models.CharField(blank=True, unique=True, max_length=8)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('sku', models.CharField(unique=True, max_length=8, blank=True)),
                 ('name', models.CharField(unique=True, max_length=32)),
-                ('slug', models.SlugField(blank=True, unique=True, max_length=32)),
-                ('teaser', models.CharField(blank=True, max_length=128)),
+                ('slug', models.SlugField(unique=True, max_length=32, blank=True)),
+                ('teaser', models.CharField(max_length=128, blank=True)),
                 ('description', models.TextField()),
                 ('attributes', jsonfield.fields.JSONField()),
                 ('created', models.DateTimeField(auto_now_add=True)),
@@ -27,5 +42,11 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='cart',
+            name='product',
+            field=models.ForeignKey(to='shop.Product'),
+            preserve_default=True,
         ),
     ]
--- gossamer/shop/models.py
+++ gossamer/shop/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
