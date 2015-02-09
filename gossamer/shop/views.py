23d7bcdcdda250ca02c2360d5b9acb075483caf7
Jared Contrascere <jcontra@gmail.com>
2015-02-09 16:56:57 -0500

    Update view and template to display entire product catalog.

--- gossamer/shop/views.py
+++ gossamer/shop/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'shop/home.html')
+    products = Product.objects.all()
+    return render_to_response('shop/home.html', {'products': products})
 
 def product_page(request):
     return render(request, 'shop/product.html')
