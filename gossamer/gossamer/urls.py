e2015a72d1f9463d88dcf9a93193fd309172154f
Jared Contrascere <jcontra@gmail.com>
2015-02-08 21:34:13 -0500

    Create initial product template.

--- gossamer/gossamer/urls.py
+++ gossamer/gossamer/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'shop.views.home_page', name='home'),
+    url(r'^product/', 'shop.views.product_page', name='product'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- gossamer/shop/views.py
+++ gossamer/shop/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     return render(request, 'shop/home.html')
+
+def product_page(request):
+    return render(request, 'shop/product.html')
