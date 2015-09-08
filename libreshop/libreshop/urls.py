a16cf870e39ffcda14f06eeda786b89e304a034f
Jared Contrascere <jcontra@gmail.com>
2015-09-07 21:27:19 -0400

    Add Bootstrap 3 static files to Django project.

--- libreshop/libreshop/urls.py
+++ libreshop/libreshop/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'shop.views.home_page', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
