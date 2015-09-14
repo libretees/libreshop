be0aae8de2209750cb3e1a412f55c7d99fd08e4a
Jared Contrascere <jcontra@gmail.com>
2015-09-13 21:25:44 -0400

    Move CustomerRegistrationForm to customers.forms module.

--- libreshop/customers/forms.py
+++ libreshop/customers/forms.py
@@ -1,14 +1,26 @@
+# Import Python module(s)
+import string
+import time
+import random
+import base64
+import hashlib
+import logging
+
 from django import forms
 from django.core import serializers
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.db.models.fields.related import ManyToManyRel
 
+from captcha.image import ImageCaptcha
+
 from .models import Customer
 from shop.models import Product, Cart
 
+# Initialize logger
+logger = logging.getLogger(__name__)
 
 class CustomerChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
@@ -63,3 +75,38 @@ class CustomerAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (('Cart'), {'fields': ('selected_products',)}),
     )
+
+
+class CustomerRegistrationForm(UserCreationForm):
+
+    captcha_response = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerRegistrationForm, self).__init__(*args, **kwargs)
+        seed = random.Random(int(round(time.time() * 1000)))
+        random.seed(seed)
+        self.token = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(6))
+        hash_object = hashlib.sha256(self.token.encode())
+        hex_dig = hash_object.hexdigest()
+
+        captcha = ImageCaptcha()
+        image = captcha.generate(self.token)
+        encoding = base64.b64encode(image.getvalue()).decode()
+        self.image = 'data:image/png;base64,%s' % encoding
+
+        self.fields['registration_token'] = forms.CharField(initial=hex_dig, widget=forms.HiddenInput())
+
+    def clean(self):
+        cleaned_data = super(CustomerRegistrationForm, self).clean()
+
+        registration_token = cleaned_data.get("registration_token")
+        captcha_response = cleaned_data.get("captcha_response")
+
+        captcha_hash = hashlib.sha256(captcha_response.encode())
+        captcha_hash = captcha_hash.hexdigest()
+        print(registration_token, captcha_hash)
+
+        if captcha_hash != registration_token:
+            forms.ValidationError('Captcha is incorrect')
+
+        return cleaned_data
