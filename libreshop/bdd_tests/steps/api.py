f82c97742d9e2f425c5e793c02aae137daca33f0
Jared Contrascere <jcontra@gmail.com>
2015-09-16 16:17:25 -0400

    Change HTTP response codes to user-friendly values.

--- libreshop/bdd_tests/steps/api.py
+++ libreshop/bdd_tests/steps/api.py
@@ -1,4 +1,5 @@
 import requests
+from rest_framework import status
 from behave import when, given, then
 
 
@@ -22,7 +23,7 @@ def step_impl(context):
 
     usernames = [result['username'] for result in response_content['results']]
 
-    context.test.assertEqual(response.status_code, 200)
+    context.test.assertEqual(response.status_code, status.HTTP_200_OK)
     context.test.assertIn(context.username, usernames)
     
 
@@ -35,5 +36,5 @@ def step_impl(context):
     authentication_errors = ['You do not have permission to perform this action.',
                              'Invalid username/password.',]
 
-    context.test.assertEqual(response.status_code, 403)
+    context.test.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     context.test.assertIn(response_content['detail'], authentication_errors)
