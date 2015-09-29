8529e12b84535eb6f01b7dd90471c8ef71775a39
Jared Contrascere <jcontra@gmail.com>
2015-09-29 19:56:49 -0400

    Refactor OAuth2 functional tests.

--- libreshop/bdd_tests/steps/oauth.py
+++ libreshop/bdd_tests/steps/oauth.py
@@ -37,38 +37,40 @@ def step_impl(context, app):
     """
     from selenium.webdriver.common.action_chains import ActionChains
     from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions
+    from selenium.webdriver.support.expected_conditions import element_to_be_clickable
     from selenium.webdriver.common.by import By
 
     if context.id_provider == 'facebook':
         # Locate the applicaiton <div> and the `X` button.
-        div = context.browser.find_element_by_xpath("(//div[contains(text(), '%s')]/ancestor::div[contains(@class, 'clearfix')])[last()-1]" % app)
-        remove_button = context.browser.find_element_by_xpath("(//div[contains(text(), '%s')]//preceding::a[@aria-label='Remove'])[last()]/i" % app)
+        xpath = (("(//div[contains(text(), '%s')]" +
+                  "/ancestor::div[contains(@class, 'clearfix')])[last()-1]") % app)
+        div = context.browser.find_element_by_xpath(xpath)
+        xpath = (("(//div[contains(text(), '%s')]" +
+                  "//preceding::a[@aria-label='Remove'])[last()]/i") % app)
+        remove_button = context.browser.find_element_by_xpath(xpath)
 
         # Hover over the application <div> element so that the `X` button displays, then click the `X` button.
         ActionChains(context.browser).move_to_element(div).click(remove_button).perform()
 
-        # Wait up to 60 seconds for the modal dialog to display.
+        # Click the `Remove` button in the modal dialog.
         wait = WebDriverWait(context.browser, 60)
-        element = wait.until(expected_conditions.presence_of_element_located((By.XPATH,"//span[contains(text(), 'Remove %s')]" % app)))
-
-        # Click the `Remove` button.
-        confirmation_button = context.browser.find_element_by_xpath("//input[@value='Remove']")
+        xpath = "//input[@value='Remove']"
+        confirmation_button = wait.until(element_to_be_clickable((By.XPATH, xpath)))
         confirmation_button.click()
 
     elif context.id_provider == 'github':
         # Locate the `Revoke` link and click it.
-        revoke_link = (("//a[contains(text(), '%s')]" +
-                        "/parent::div" +
-                        "/following-sibling::div" +
-                        "/a[contains(text(),'Revoke')]") % app)
-        revoke_link = context.browser.find_element_by_xpath(revoke_link)
+        xpath = (("//a[contains(text(), '%s')]" +
+                  "/parent::div" +
+                  "/following-sibling::div" +
+                  "/a[contains(text(),'Revoke')]") % app)
+        revoke_link = context.browser.find_element_by_xpath(xpath)
         revoke_link.click()
 
         # Confirm that we wish to revoke access.
         wait = WebDriverWait(context.browser, 60)
-        revoke_button = ("//h2[contains(text(), 'Are you sure you want to revoke authorization?')]" +
-                         "/ancestor::div[@role='dialog']" +
-                         "//button[contains(text(), 'I understand, revoke access')]")
-        revoke_button = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, revoke_button)))
+        xpath = ("//h2[contains(text(), 'Are you sure you want to revoke authorization?')]" +
+                 "/ancestor::div[@role='dialog']" +
+                 "//button[contains(text(), 'I understand, revoke access')]")
+        revoke_button = wait.until(element_to_be_clickable((By.XPATH, xpath)))
         revoke_button.click()
