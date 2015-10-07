290c92e0dd2a3e35bd4517274b25e5f58fad76d9
Jared Contrascere <jcontra@gmail.com>
2015-10-07 15:34:59 -0400

    Refactor VariantFormFactory into generic PopulatedFormFactory.

--- libreshop/products/admin.py
+++ libreshop/products/admin.py
@@ -1,21 +1,18 @@
-import types
 import logging
 from django.contrib import admin
 from django.contrib.admin.options import IS_POPUP_VAR
-from django import forms
-from django.forms.widgets import Select
-from .models import Product, Variant, Component, Inventory, Location, Attribute, Attribute_Value
-from .forms import ProductCreationForm, ProductChangeForm
+from . import models
+from .forms import ProductCreationForm, ProductChangeForm, PopulatedFormFactory
 
 # Initialize logger
 logger = logging.getLogger(__name__)
 
 # Register your models here.
-admin.site.register(Component)
-admin.site.register(Inventory)
-admin.site.register(Location)
-admin.site.register(Attribute)
-admin.site.register(Attribute_Value)
+admin.site.register(models.Component)
+admin.site.register(models.Inventory)
+admin.site.register(models.Location)
+admin.site.register(models.Attribute)
+admin.site.register(models.Attribute_Value)
 
 class ProductAdmin(admin.ModelAdmin):
 
@@ -67,43 +64,6 @@ class ProductAdmin(admin.ModelAdmin):
                                                       post_url_continue)
 
 
-def VariantFormFactory(request):
-
-    model_field_names = [_.name for _ in Variant._meta.get_fields()]
-
-    class VariantForm(forms.ModelForm):
-        def __init__(self, *args, **kwargs):
-            super(VariantForm, self).__init__(*args, **kwargs)
-            if request.method == 'GET':
-                # Load initial form fields from GET parameters
-                for key in request.GET:
-                    try:
-                        self.fields[key].required = False
-                        self.fields[key].widget.attrs['readonly'] = True
-                        self.fields[key].widget.attrs['disabled'] = 'disabled'
-                    except KeyError:
-                        # Ignore unexpected parameters
-                        pass
-            else:
-                prepopulated_fields = ([name for name in model_field_names
-                                          if name in request.GET
-                                          and name not in request.POST])
-                for name in prepopulated_fields:
-                    self.fields[name].required = False
-
-    def get_clean_function(name):
-        def func(self):
-            return (Product.objects.get(id=request.GET[name])
-                if isinstance(self.fields[name], forms.models.ModelChoiceField)
-                else request.GET[name])
-        return func
-
-    for key in [key for key in request.GET if key in model_field_names]:
-        setattr(VariantForm, 'clean_%s' % key, get_clean_function(key))
-
-    return VariantForm
-
-
 class VariantAdmin(admin.ModelAdmin):
 
     def __init__(self, *args, **kwargs):
@@ -112,17 +72,10 @@ class VariantAdmin(admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         defaults = {}
         if obj is None:
-            defaults['form'] = VariantFormFactory(request)
+            defaults['form'] = PopulatedFormFactory(models.Variant, request)
         defaults.update(kwargs)
         return super(VariantAdmin, self).get_form(request, obj, **defaults)
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name in request.GET:
-            kwargs['queryset'] = Product.objects.filter(id=request.GET[db_field.name])
-            kwargs['widget'] = Select(attrs={'disabled': 'disabled'})
-            kwargs['required'] = False
-        return super(VariantAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Variant, VariantAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Variant, VariantAdmin)
--- libreshop/products/forms.py
+++ libreshop/products/forms.py
@@ -2,11 +2,12 @@ import re
 import logging
 from xml.etree import ElementTree
 from django import forms
+from django.forms.widgets import Select
 from django.contrib import admin
 from django.db.models.fields.related import OneToOneRel
 from django.utils.safestring import mark_safe
 
-from .models import Product, Variant
+from . import models
 
 # Initialize logger
 logger = logging.getLogger(__name__)
@@ -44,10 +45,10 @@ class ProductChangeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProductChangeForm, self).__init__(*args, **kwargs)
-        self.fields['variants'].queryset = Variant.objects.filter(product=self.instance)
-        self.initial['variants'] = Variant.objects.filter(product=self.instance)
-        relation = OneToOneRel(field=Product,
-                               to=Variant,
+        self.fields['variants'].queryset = models.Variant.objects.filter(product=self.instance)
+        self.initial['variants'] = models.Variant.objects.filter(product=self.instance)
+        relation = OneToOneRel(field=models.Product,
+                               to=models.Variant,
                                field_name='id')
 
         prepopulated_values = [('product', self.instance.pk)]
@@ -60,15 +61,15 @@ class ProductChangeForm(forms.ModelForm):
         instance = super(ProductChangeForm, self).save(*args, **kwargs)
 
         if instance.pk:
-            for variant in [variant for variant in Variant.objects.filter(product=instance.pk)]:
+            for variant in [variant for variant in models.Variant.objects.filter(product=instance.pk)]:
                 if variant not in self.cleaned_data['variants']:
                     # remove a variant that has been unselected
-                    Variant.objects.get(id=variant.id).delete()
+                    models.Variant.objects.get(id=variant.id).delete()
 
         return instance
 
     class Meta:
-        model = Product
+        model = models.Product
         fields = ('sku',)
 
 
@@ -77,5 +78,58 @@ class ProductCreationForm(forms.ModelForm):
         super(ProductCreationForm, self).__init__(*args, **kwargs)
 
     class Meta:
-        model = Product
+        model = models.Product
         fields = ('sku',)
+
+
+def PopulatedFormFactory(cls, request):
+
+    model_field_names = [_.name for _ in cls._meta.get_fields()]
+
+    class PopulatedForm(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super(PopulatedForm, self).__init__(*args, **kwargs)
+            if request.method == 'GET':
+                for key in request.GET:
+                    try:
+                        field = self.fields[key]
+                        field.required = False
+                        if isinstance(field, forms.models.ModelChoiceField):
+                            related_model_name = (field.queryset.model._meta
+                                .model_name.title())
+                            cls = getattr(models, related_model_name, None)
+                            if cls:
+                                object_id = request.GET[key]
+                                field.widget = (Select(
+                                    attrs={'disabled': 'disabled'}))
+                                field.queryset = (cls.objects.
+                                    filter(id=object_id))
+                        else:
+                            field.widget.attrs['readonly'] = True
+                            field.widget.attrs['disabled'] = 'disabled'
+                    except KeyError:
+                        # Ignore unexpected parameters
+                        pass
+            else:
+                prepopulated_fields = ([name for name in model_field_names
+                    if name in request.GET
+                    and name not in request.POST])
+                for name in prepopulated_fields:
+                    self.fields[name].required = False
+
+    def get_clean_function(name):
+        def func(self):
+            value = None
+            field = self.fields[name]
+            if isinstance(field, forms.models.ModelChoiceField):
+                related_model = field.queryset.model
+                value = related_model.objects.get(id=request.GET[name])
+            else:
+                value = request.GET[name]
+            return value
+        return func
+
+    for key in [key for key in request.GET if key in model_field_names]:
+        setattr(PopulatedForm, 'clean_%s' % key, get_clean_function(key))
+
+    return PopulatedForm
