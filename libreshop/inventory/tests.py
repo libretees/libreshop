a0fc70f987f2880749537aa2cba14ed455930410
Jared Contrascere <jcontra@gmail.com>
2015-10-14 14:44:01 -0400

    Add unit tests to ensure that an Inventory object cannot have two values for the same Attribute.

--- libreshop/inventory/tests.py
+++ libreshop/inventory/tests.py
@@ -1,5 +1,6 @@
 import logging
 from decimal import Decimal
+from django.db.utils import IntegrityError
 from django.test import TestCase
 from shop.models import Address
 from inventory.models import (Warehouse, Attribute, Inventory, Attribute_Value,
@@ -49,6 +50,41 @@ class AttributeValueModelTest(TestCase):
 
         num_attribute_values = Attribute_Value.objects.all().count()
         self.assertEqual(num_attribute_values, 1)
+
+
+    def test_inventory_can_have_multiple_unique_attributes(self):
+        '''
+        Test that multiple distinct Attributes can be associated to an Inventory
+        object.
+        '''
+        inventory = Inventory.objects.create(name='foo')
+        attribute1 = Attribute.objects.create(name='bar')
+        attribute2 = Attribute.objects.create(name='baz')
+        attribute_value1 = Attribute_Value.objects.create(attribute=attribute1,
+            inventory=inventory, value='qux')
+        attribute_value2 = Attribute_Value.objects.create(attribute=attribute2,
+            inventory=inventory, value='quux')
+
+        num_associated_attributes = (Attribute_Value.objects.
+            filter(inventory=inventory).count())
+
+        self.assertEqual(num_associated_attributes, 2)
+
+
+    def test_inventory_cannot_have_multiple_similar_attributes(self):
+        '''
+        Test that multiple similar Attributes cannot be associated to an
+        Inventory object.
+        '''
+        inventory = Inventory.objects.create(name='foo')
+        attribute = Attribute.objects.create(name='bar')
+        attribute_value1 = Attribute_Value.objects.create(attribute=attribute,
+            inventory=inventory, value='baz')
+
+        func = Attribute_Value.objects.create
+
+        self.assertRaises(IntegrityError, func, attribute=attribute,
+            inventory=inventory, value='qux')
 
 
 class LocationModelTest(TestCase):
