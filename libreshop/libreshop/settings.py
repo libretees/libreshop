304b4bfb8831da85512697629561da359058355a
Jared Contrascere <jcontra@gmail.com>
2015-10-27 19:41:27 -0400

    Create project-wide base template.

--- libreshop/libreshop/settings.py
+++ libreshop/libreshop/settings.py
@@ -121,7 +121,7 @@ ROOT_URLCONF = 'libreshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'libreshop/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
