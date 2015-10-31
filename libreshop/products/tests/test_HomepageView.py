47af38d9567f468fc3b02c91d99b9c7c97c11c12
Jared Contrascere <jcontra@gmail.com>
2015-10-31 12:50:06 -0400

    Refactor products.views.HomepageView to extend from django.views.generic.FormView.

--- libreshop/products/tests/test_HomepageView.py
+++ libreshop/products/tests/test_HomepageView.py
@@ -1,9 +1,12 @@
-import logging
+from importlib import import_module
+from django.http import HttpRequest
+from django.conf import settings
 from django.test import TestCase
+from carts import SessionCart
+from products.forms import ProductOrderForm
+from products.models import Product, Variant
 from ..views import HomepageView
 
-# Initialize logger.
-logger = logging.getLogger(__name__)
 
 class HomepageViewTest(TestCase):
 
@@ -11,15 +14,72 @@ class HomepageViewTest(TestCase):
         '''
         Test that the view uses the products/featured.html template.
         '''
+        product = Product.objects.create(name='foo', sku='1000')
         response = self.client.get('')
         self.assertTemplateUsed(response, 'products/featured.html')
 
 
-    def test_template_includes_csrf_token(self):
+    def test_view_template_includes_csrf_token(self):
         '''
         Test that the view's template includes a CSRF token.
         '''
+        product = Product.objects.create(name='foo', sku='1000')
         response = self.client.get('')
         csrf_token_regex = "<input type='hidden' name='csrfmiddlewaretoken' value='[A-Za-z0-9]{32}' />"
         rendered_html = response.content.decode()
         self.assertRegex(rendered_html, csrf_token_regex)
+
+
+    def test_view_uses_product_order_form(self):
+        '''
+        Test that the FormView uses the ProductOrderForm Form.
+        '''
+        product = Product.objects.create(name='foo', sku='1000')
+
+        request = HttpRequest()
+        homepage_view = HomepageView()
+        homepage_view.request = request
+        form = homepage_view.get_form()
+        self.assertIsInstance(form, ProductOrderForm)
+
+
+    def test_view_adds_variant_to_cart_when_form_is_valid(self):
+        '''
+        Test that a Variant is added to a SessionCart when valid POST data is
+        submitted.
+        '''
+        product = Product.objects.create(name='foo', sku='1000')
+
+        request = HttpRequest()
+        request.method = 'POST'
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = None
+        request.session = engine.SessionStore(session_key)
+
+        homepage_view = HomepageView()
+        homepage_view.request = request
+        form = homepage_view.get_form()
+        form.full_clean()
+        form = homepage_view.form_valid(form)
+
+        cart = SessionCart(request.session)
+
+        self.assertEqual(cart.count, 1)
+
+
+    def test_view_context_includes_session_cart(self):
+        '''
+        Test that the context provided by the FormView includes a SessionCart.
+        '''
+        request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = None
+        request.session = engine.SessionStore(session_key)
+
+        homepage_view = HomepageView()
+        homepage_view.request = request
+        context = homepage_view.get_context_data()
+
+        cart = context.get('cart', None)
+
+        self.assertIsInstance(cart, SessionCart)
--- libreshop/products/views.py
+++ libreshop/products/views.py
@@ -1,26 +1,48 @@
 import logging
-from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic.base import TemplateView
+from django.views.generic import FormView
+from carts import SessionCart
 from products.forms import ProductOrderForm
-from products.models import Product
+from products.models import Product, Variant
 
-# Initialize logger.
 logger = logging.getLogger(__name__)
 
 # Create your views here.
-class HomepageView(TemplateView):
+class HomepageView(FormView):
 
     template_name = 'products/featured.html'
+    success_url = '/'
+
+    def get_form(self):
+        product = Product.objects.get(sku='1000')
+        return ProductOrderForm(product, **self.get_form_kwargs())
+
+
+    def form_valid(self, form):
+        logger.info('%s is valid' % type(form).__name__)
+
+        product = Product.objects.get(sku='1000')
+        cart = SessionCart(self.request.session)
+
+        variants = Variant.objects.filter(product=product)
+        for variant in variants:
+            relevant_attributes = {
+                key:variant.attributes[key]
+                for key in variant.attributes
+                if key in form.cleaned_data
+            }
+            if relevant_attributes == form.cleaned_data:
+                logger.info('User selected "%s"' % variant)
+                cart.add(variant.id)
+                break
+
+        return super(HomepageView, self).form_valid(form)
+
 
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data(**kwargs)
-        product = None
-        try:
-            product = Product.objects.get(sku='1000') or None
-        except ObjectDoesNotExist as e:
-            logger.info('Featured product does not exist.')
 
-        if product:
-            context['product'] = product
-            context['form'] = ProductOrderForm(product)
+        context.update({
+            'cart': SessionCart(self.request.session)
+        })
+
         return context
