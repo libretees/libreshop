c35d4e5778332448a0cea398152483078ef426cb
Jared Contrascere <jcontra@gmail.com>
2015-10-31 14:03:51 -0400

    Refactor carts.sessioncart.SessionCart to extend from list.

--- libreshop/carts/sessioncart.py
+++ libreshop/carts/sessioncart.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 
 UUID = 'b37d96da-006e-4ba1-945c-ca4f3b318eea'
 
-class SessionCart(object):
+class SessionCart(list):
 
     def __init__(self, session):
 
@@ -12,7 +12,7 @@ class SessionCart(object):
 
         if not session.has_key(UUID):
             logger.info('Could not find a %s!' % self.__class__.__name__)
-            session[UUID] = []
+            session[UUID] = self
         else:
             logger.info('Found %s!' % self.__class__.__name__)
 
@@ -23,7 +23,7 @@ class SessionCart(object):
         logger.debug('request.session: %s' % self.session[UUID])
         logger.info('Adding product to %s...' % self.__class__.__name__)
 
-        self.session[UUID].append(product_id)
+        self.append(product_id)
         self.session.modified = True
 
         logger.info('Added product to %s.' % self.__class__.__name__)
@@ -32,9 +32,9 @@ class SessionCart(object):
 
     @property
     def count(self):
-        return len(self.session.get(UUID)) if self.session.has_key(UUID) else 0
+        return len(self)
 
 
     @property
     def has_products(self):
-        return self.count > 0
+        return bool(len(self))
