1ba9ef2901ec2ff0246fa81d22f8126c2078e9b2
Jared Contrascere <jcontra@gmail.com>
2015-11-03 16:19:30 -0500

    Make carts.utils.SessionList more DRY by adding carts.utils.session_based class wrapper.

--- libreshop/carts/utils.py
+++ libreshop/carts/utils.py
@@ -1,11 +1,32 @@
 import logging
+from functools import wraps
 
 logger = logging.getLogger(__name__)
 
 UUID = 'b37d96da-006e-4ba1-945c-ca4f3b318eea'
 
-class SessionList(list):
+def session_based(cls):
 
+    methods = ['__setitem__', '__delitem__', '__iadd__', '__imul__', 'pop',
+        'append', 'extend', 'insert', 'remove', 'clear', 'sort', 'reverse',
+    ]
+
+    def wrap_method(method):
+        def wrapper(self, *args, **kwargs):
+            value = method(self, *args, **kwargs)
+            self._update_session()
+            return value
+        return wrapper
+
+    for method_name in methods:
+        method = wrap_method(getattr(cls, method_name))
+        setattr(cls, method_name, method)
+
+    return cls
+
+
+@session_based
+class SessionList(list):
     def __init__(self, session, *args, **kwargs):
         super(SessionList, self).__init__(*args, **kwargs)
 
@@ -19,64 +40,3 @@ class SessionList(list):
 
     def _update_session(self):
         self.session[UUID] = list(self)
-
-
-    def __setitem__(self, key, value):
-        super(SessionList, self).__setitem__(key, value)
-        self._update_session()
-
-
-    def __delitem__(self, key):
-        super(SessionList, self).__delitem__(key)
-        self._update_session()
-
-
-    def __iadd__(self, L):
-        super(SessionList, self).__iadd__(L)
-        self._update_session()
-
-
-    def __imul__(self, i):
-        super(SessionList, self).__imul__(i)
-        self._update_session()
-
-
-    def append(self, x):
-        super(SessionList, self).append(x)
-        self._update_session()
-
-
-    def extend(self, L):
-        super(SessionList, self).extend(L)
-        self._update_session()
-
-
-    def insert(self, i, x):
-        super(SessionList, self).insert(i, x)
-        self._update_session()
-
-
-    def remove(self, x):
-        super(SessionList, self).remove(x)
-        self._update_session()
-
-
-    def pop(self, i):
-        i = super(SessionList, self).pop(i)
-        self._update_session()
-        return i
-
-
-    def clear(self):
-        super(SessionList, self).clear()
-        self._update_session()
-
-
-    def sort(self):
-        super(SessionList, self).sort()
-        self._update_session()
-
-
-    def reverse(self):
-        super(SessionList, self).reverse()
-        self._update_session()
