da8b0a791f094f88aae433ab608965df3b12241b
Jared Contrascere <jcontra@gmail.com>
2015-11-10 20:12:38 -0500

    Implement carts.views.RemoveItemView to remove items from the session-based cart.

--- libreshop/carts/tests.py
+++ libreshop/carts/tests.py
@@ -1,7 +1,10 @@
 from importlib import import_module
-from django.shortcuts import resolve_url
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpRequest
 from django.test import TestCase
 from .utils import SessionList
+from .views import RemoveItemView
 
 # Create your tests here.
 class SessionListTest(TestCase):
@@ -234,9 +237,61 @@ class RemoveItemViewTest(TestCase):
         Test that the view redirects to the Home Page, by default, as part of
         the Post/Redirect/Get (PRG) flow, when the 'next' POST variable not set.
         '''
-        url = resolve_url('remove_item')
-        home_url = resolve_url('home')
+        url = reverse('remove_item')
+        home_url = reverse('home')
 
         response = self.client.post(url, {})
 
         self.assertRedirects(response, home_url)
+
+
+    def test_view_redirects_to_location_set_in_next_post_variable(self):
+        '''
+        Test that the view redirects to the location set in the 'next' POST
+        variable, as part of the Post/Redirect/Get (PRG) flow.
+        '''
+        url = reverse('remove_item')
+
+        response = self.client.post(url, {'next': 'http://www.example.com'})
+
+        self.assertRedirects(response, 'http://www.example.com')
+
+
+    def test_view_removes_item_in_cart_set_in_remove_post_variable(self):
+        '''
+        Test that the view removes the item within the cart at the position
+        contained in the 'remove' POST variable.
+        '''
+        request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = None
+        request.session = engine.SessionStore(session_key)
+        url = reverse('remove_item')
+        session_list1 = SessionList(request.session, ['foo', 'bar'])
+        request.POST['remove'] = '1'
+
+        view = RemoveItemView()
+        response = view.post(request)
+
+        session_list2 = SessionList(request.session)
+        self.assertEqual(session_list2, ['foo'])
+
+
+    def test_view_does_not_affect_cart_when_remove_post_variable_is_invalid(self):
+        '''
+        Test that the view does not affect the cart if the position specified in
+        the 'remove' POST variable is not valid.
+        '''
+        request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = None
+        request.session = engine.SessionStore(session_key)
+        url = reverse('remove_item')
+        session_list1 = SessionList(request.session, ['foo', 'bar'])
+        request.POST['remove'] = '3'
+
+        view = RemoveItemView()
+        response = view.post(request)
+
+        session_list2 = SessionList(request.session)
+        self.assertEqual(session_list2, ['foo', 'bar'])
--- libreshop/carts/views.py
+++ libreshop/carts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.views.generic import View
+from django.shortcuts import redirect
+from .utils import SessionList
 
 # Create your views here.
+class RemoveItemView(View):
+
+    def post(self, request):
+
+        index = request.POST.get('remove', None)
+
+        if index:
+            cart = SessionList(request.session)
+            try:
+                del cart[int(index)]
+            except IndexError as e:
+                pass
+
+        next_url = request.POST.get('next', reverse('home'))
+
+        return redirect(next_url, permanent=False)
