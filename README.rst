6e9864bfd001ebcfe8e134de1881e42c9005edfd
Jared Contrascere <jcontra@gmail.com>
2015-11-25 10:02:41 -0500

    Add setup.py and README.rst files for submission to PyPI.

---
+++ README.rst
@@ -0,0 +1,1 @@
+LibreShop is an eCommerce solution geared towards small business.
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'libreshop',
+    packages = ['libreshop'],
+    version = '0.1',
+    description = 'LibreShop provides a simple eCommerce solution for small businesses.',
+    author = 'LibreTees, LLC',
+    author_email = 'contact@libretees.com',
+    url = 'https://github.com/libretees/libreshop',
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development',
+    ],
+)
