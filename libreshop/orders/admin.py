55f02b3c82b3ed927d330467f11e00b9260da81a
Jared Contrascere <jcontra@gmail.com>
2016-01-21 20:41:53 -0500

    Update Orders ModelAdmin page to include more useful information.

--- libreshop/orders/admin.py
+++ libreshop/orders/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Order, Purchase, TaxRate
 
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'token', '_recipient', '_purchases', 'subtotal', 'sales_tax',
+        'shipping_cost', 'total', 'created', 'fulfilled'
+    )
+
+    def _purchases(self, instance):
+        return instance.purchase_count
+    _purchases.short_description = 'Purchases'
+    _purchases.admin_order_field = 'purchase_count'
+
+
+    def _recipient(self, instance):
+        return instance.shipping_address.recipient_name
+    _recipient.short_description = 'Recipient'
+    _recipient.admin_order_field = 'shipping_address__recipient_name'
+
+
+    def get_queryset(self, request):
+        queryset = super(OrderAdmin, self).get_queryset(request)
+        queryset = queryset.annotate(purchase_count=Count('purchase'))
+        return queryset
+
+
 # Register your models here.
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Purchase)
 admin.site.register(TaxRate)
