03aa7519da5ac21d3d9725d8ef7734a66cb025d7
Jared Contrascere <jcontra@gmail.com>
2016-01-31 21:24:55 -0500

    Add unit test for UniqueTogetherFormSet.

---
+++ libreshop/contrib/tests.py
@@ -0,0 +1,44 @@
+from django.core.exceptions import ValidationError
+from django.forms.models import inlineformset_factory
+from django.test import TestCase
+from .forms.models import UniqueTogetherFormSet
+from .models import Inventory, Location, Warehouse
+
+# Create your tests here.
+class UniqueTogetherFormSetTest(TestCase):
+
+    def setUp(self):
+        self.post_data = {
+            'location_set-INITIAL_FORMS': '0',
+            'location_set-TOTAL_FORMS': '2',
+            'location_set-0-id': '',
+            'location_set-0-inventory': '',
+            'location_set-0-quantity': '',
+            # 'location_set-0-warehouse': '1',
+            'location_set-1-id': '',
+            'location_set-1-inventory': '',
+            'location_set-1-quantity': '',
+            # 'location_set-1-warehouse': '1',
+        }
+
+
+    def test_formset_raises_exception_for_duplicate_values_in_intermediate_model(self):
+        '''
+        Test that UniqueTogetherFormSet raises an exception when duplicate
+        values exist for an intermediate model's Meta.unique_together field.
+        '''
+        warehouse = Warehouse.objects.create(name='foo')
+
+        self.post_data.update({
+            'location_set-0-warehouse': warehouse.pk,
+            'location_set-1-warehouse': warehouse.pk,
+        })
+
+        FormSet = inlineformset_factory(
+            Inventory, Location, formset=UniqueTogetherFormSet,
+            fields=('inventory', 'warehouse')
+        )
+
+        formset = FormSet(data=self.post_data)
+
+        self.assertRaises(ValidationError, formset.clean)
