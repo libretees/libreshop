fb12715907e28d234f9f2c6befb0c2d23bb4948d
Jared Contrascere <jcontra@gmail.com>
2016-02-07 03:15:13 -0500

    Enable order fulfillment via the 'fulfill' management command.

--- libreshop/orders/management/commands/fulfill.py
+++ libreshop/orders/management/commands/fulfill.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
 from orders.models import Order
 
 class Command(BaseCommand):
@@ -6,22 +7,36 @@ class Command(BaseCommand):
     help = 'Fulfills the specified order via drop shipment'
 
     def add_arguments(self, parser):
-        parser.add_argument('order_id', nargs='+', type=int)
+        parser.add_argument('order_tokens', nargs='*', type=str)
 
 
     def handle(self, *args, **options):
 
-        for order_id in options['order_id']:
-            try:
-                order = Order.objects.get(pk=order_id)
-            except Order.DoesNotExist:
-                raise CommandError('Order "%s" does not exist' % order_id)
+        tokens = options['order_tokens']
 
+        orders = (
+            Order.objects.filter(token__in=tokens) or
+            Order.objects.filter(fulfilled=False)
+        )
+
+        if len(orders) != len(tokens):
+            for token in tokens:
+                try:
+                    order = Order.objects.get(token=token)
+                except Order.DoesNotExist:
+                    self.stdout.write(self.style.WARNING(
+                        'Order \'%s\' was specified but does not exist.' % token
+                    ))
+
+        if not orders:
+            self.stdout.write(self.style.NOTICE(
+                'No orders were fulfilled.'
+            ))
+
+        for order in orders:
             order.fulfilled = True
             order.save()
 
-            self.stdout.write(
-                self.style.SUCCESS(
-                    'Successfully fulfilled order "%s"' % order_id
-                )
-            )
+            self.stdout.write(self.style.SUCCESS(
+                'Order %s has been fulfilled.' % order.token
+            ))
