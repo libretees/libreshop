732941869f540a7428a5806b8e4e6869246465f5
Jared Contrascere <jcontra@gmail.com>
2016-03-26 21:04:15 -0400

    Update data model for orders.TaxRate.

--- libreshop/orders/models.py
+++ libreshop/orders/models.py
@@ -88,12 +88,11 @@ class Purchase(TimeStampedModel):
 
 class TaxRate(TimeStampedModel):
 
-    state = models.CharField(max_length=4, null=True, blank=True)
-    district = models.CharField(max_length=4, null=True, blank=True)
-    county = models.CharField(max_length=4, null=True, blank=True)
-    city = models.CharField(max_length=4, null=True, blank=True)
+    state = models.CharField(max_length=64, null=True, blank=True)
+    district = models.CharField(max_length=64, null=True, blank=True)
+    county = models.CharField(max_length=64, null=True, blank=True)
+    city = models.CharField(max_length=64, null=True, blank=True)
     postal_code = models.CharField(max_length=16, null=True, blank=True)
-
 
     state_tax_rate = models.DecimalField(max_digits=5, decimal_places=4,
         null=False, blank=False, default=Decimal('0.00'))
