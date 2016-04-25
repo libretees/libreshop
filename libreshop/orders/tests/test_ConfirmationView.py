c552880c68dd996ce4839abdbcbffdae064d0551
Jared Contrascere <jcontra@gmail.com>
2016-04-24 20:22:14 -0400

    Add additional unit test to orders.views.ConfirmationView.

--- libreshop/orders/tests/test_ConfirmationView.py
+++ libreshop/orders/tests/test_ConfirmationView.py
@@ -3,30 +3,55 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.core.urlresolvers import reverse
 from django.test import TestCase, RequestFactory
-from orders.models import Order, Purchase
+from orders.models import Order, Purchase, Transaction
 from products.models import Product, Variant
 from ..views import ConfirmationView, UUID
 
 class ConfirmationViewTest(TestCase):
 
-    def setUp(self):
+    def create_http_request(self, method, data=None):
         '''
-        Create common test assets prior to each individual unit test run.
         '''
-        # Set up HTTP request.
         factory = RequestFactory()
-        self.request = factory.get(reverse('checkout:confirmation'))
+
+        http_method = getattr(factory, method.lower())
+        self.request = http_method(reverse('checkout:confirmation'), data=data)
+
+        # Set `user` manually, since middleware is not supported.
+        self.request.user = AnonymousUser()
 
         # Set `session` manually, since middleware is not supported.
         engine = import_module(settings.SESSION_ENGINE)
         session_key = None
         self.request.session = engine.SessionStore(session_key)
 
-        # Set `user` manually, since middleware is not supported.
-        self.request.user = AnonymousUser()
+        # Add Order Token to session variables.
+        self.request.session[UUID] = {}
+        self.request.session[UUID]['order_token'] = self.order.token
+        self.request.session.modified = True
 
+
+    def setUp(self):
+        '''
+        Create common test assets prior to each individual unit test run.
+        '''
         # Set up test data.
+        self.product = Product.objects.create(name='foo', sku='123')
+        self.variant = Variant.objects.create(
+            product=self.product, name='bar', sub_sku='456'
+        )
+        self.order = Order.objects.create()
+        self.purchase = Purchase.objects.create(
+            order=self.order, variant=self.variant
+        )
+        self.transaction = Transaction.objects.create(
+            order=self.order, transaction_id='foo'
+        )
         self.view = ConfirmationView.as_view()
+
+        # Set up HTTP request.
+        self.create_http_request('GET')
+
 
 
     def test_view_returns_200_status_if_no_order_token_is_in_session_variables(self):
@@ -34,6 +59,9 @@ class ConfirmationViewTest(TestCase):
         Test that the ConfirmationView returns a 200 OK status if there is no
         Order Token within session variables.
         '''
+        # Delete session variable.
+        del self.request.session[UUID]['order_token']
+
         response = self.view(self.request)
         response = response.render()
         rendered_html = response.content.decode()
@@ -46,22 +74,24 @@ class ConfirmationViewTest(TestCase):
         Test that the ConfirmationView returns a 200 OK status if an Order Token
         is present within session variables.
         '''
-        # Set up Product, Variant, Order, and Purchase.
-        product = Product.objects.create(name='foo', sku='123')
-        variant = Variant.objects.create(
-            product=product, name='bar', sub_sku='456'
-        )
-        order = Order.objects.create()
-        purchase = Purchase.objects.create(order=order, variant=variant)
-
-        # Add Order Token to session variables.
-        self.request.session[UUID] = {}
-        self.request.session[UUID]['order_token'] = order.token
-        self.request.session.modified = True
-
         # Perform test.
         response = self.view(self.request)
         response = response.render()
         rendered_html = response.content.decode()
 
         self.assertEqual(response.status_code, 200)
+
+
+    def test_view_redirects_on_successful_post(self):
+        '''
+        Test that the ConfirmationView returns a 302 Found (Temporary Redirect)
+        status if valid Form data is POSTed to the View's OrderReceiptForm.
+        '''
+        # Set up HTTP POST request.
+        data = {'email_address': 'test@example.com'}
+        self.create_http_request('POST', data=data)
+
+        # Perform test.
+        response = self.view(self.request, **data)
+
+        self.assertEqual(response.status_code, 302)
