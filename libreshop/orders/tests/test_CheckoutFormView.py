111d834526d0e567946d193d806094877131c945
Jared Contrascere <jcontra@gmail.com>
2016-04-27 22:09:54 -0400

    Add additional unit test for orders.views.CheckoutFormView.

--- libreshop/orders/tests/test_CheckoutFormView.py
+++ libreshop/orders/tests/test_CheckoutFormView.py
@@ -1,9 +1,11 @@
+from decimal import Decimal
 from importlib import import_module
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.core.urlresolvers import reverse
 from django.test import TestCase, RequestFactory
 from orders.models import Order, Purchase, Transaction
+from carts.utils import SessionCart
 from products.models import Product, Variant
 from ..views import CheckoutFormView, UUID
 
@@ -18,7 +20,7 @@ class CheckoutFormViewTest(TestCase):
         factory = RequestFactory()
 
         http_method = getattr(factory, method.lower())
-        self.request = http_method(reverse('checkout:confirmation'), data=data)
+        self.request = http_method(reverse('checkout:main'), data=data)
 
         # Set `user` manually, since middleware is not supported.
         self.request.user = AnonymousUser()
@@ -34,6 +36,16 @@ class CheckoutFormViewTest(TestCase):
         Create common test assets prior to each individual unit test run.
         '''
         # Set up test data.
+        product = Product.objects.create(name='foo', sku='123')
+        variant = product.variant_set.first()
+        variant.price = Decimal(12.34)
+        variant.sub_sku = '456'
+        variant.save()
+        self.variant = variant
+        self.variant2 = Variant.objects.create(
+            product=product, name='bar', price=Decimal(43.21), sub_sku='789'
+        )
+
         self.view = CheckoutFormView.as_view()
 
         # Set up HTTP request.
@@ -43,10 +55,49 @@ class CheckoutFormViewTest(TestCase):
     def test_view_returns_200_status_if_no_variants_are_in_cart(self):
         '''
         Test that the CheckoutFormView returns a 200 OK status if there are no
-        Variants within a SessionCart.
+        Variants within the SessionCart.
         '''
         response = self.view(self.request)
         response = response.render()
         rendered_html = response.content.decode()
 
         self.assertEqual(response.status_code, 200)
+
+
+    def test_view_returns_200_status_if_variants_are_in_cart(self):
+        '''
+        Test that the CheckoutFormView returns a 200 OK status if there are
+        Variants the SessionCart.
+        '''
+        cart = SessionCart(self.request.session)
+        cart.add(self.variant)
+
+        response = self.view(self.request)
+        response = response.render()
+        rendered_html = response.content.decode()
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_initially_redirects_to_self(self):
+        '''
+        Test that the CheckoutFormView initially redirects to itself.
+        '''
+        # Set up HTTP POST request.
+        request_data = {
+            'recipient_name': 'Foo Bar',
+            'street_address': '123 Test St',
+            'locality': 'Test',
+            'region': 'OK',
+            'postal_code': '12345',
+            'country': 'US'
+        }
+        self.create_http_request('POST', data=request_data)
+
+        cart = SessionCart(self.client.session)
+        cart.add(self.variant)
+
+        response = self.view(self.request, **request_data)
+
+        view_url = reverse('checkout:main')
+        self.assertEqual(response.url, view_url)
