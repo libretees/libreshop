a521624b275606bcaa3d2ade3a4a634b72cca3b8
Jared Contrascere <jcontra@gmail.com>
2016-10-20 14:34:28 -0400

    Dynamically calculate fields for inventory.PurchaseOrder model.

--- libreshop/inventory/admin.py
+++ libreshop/inventory/admin.py
@@ -27,12 +27,39 @@ class SupplyAdmin(admin.TabularInline):
     min_num = 1
     extra = 0
 
+
 @admin.register(models.PurchaseOrder)
 class PurchaseOrder(admin.ModelAdmin):
     inlines = [SupplyAdmin]
     list_display = (
-        'number', 'submitted', 'subtotal', 'sales_tax', 'shipping_cost', 'total'
+        'number', 'submitted', '_subtotal', 'sales_tax', 'shipping_cost',
+        '_total', '_status'
     )
+
+    def _subtotal(self, instance):
+        return instance.subtotal
+    _subtotal.short_description = 'Subtotal'
+    _subtotal.admin_order_field = 'subtotal'
+
+
+    def _total(self, instance):
+        return instance.total
+    _total.short_description = 'Total'
+    _total.admin_order_field = 'total'
+
+
+    def _status(self, instance):
+        status = None
+        if instance.percent_received == 0:
+            status = 'Open'
+        elif instance.percent_received < 1:
+            status = 'Partially Received'
+        else:
+            status = 'Closed'
+        return status
+    _status.short_description = 'Status'
+    _status.admin_order_field = 'percent_received'
+
 
 admin.site.register(models.Inventory, InventoryAdmin)
 admin.site.register(models.Location)
--- libreshop/inventory/models.py
+++ libreshop/inventory/models.py
@@ -3,8 +3,9 @@ from decimal import Decimal
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import (
+    Case, Count, ExpressionWrapper, F, Sum, DecimalField, IntegerField, When)
 from django.utils import timezone
-
 from django_measurement.models import MeasurementField
 from measurement.measures import Weight
 from model_utils.models import TimeStampedModel
@@ -136,7 +137,12 @@ class Supply(TimeStampedModel):
         verbose_name_plural = 'Supplies'
 
     inventory = models.ForeignKey('Inventory', null=True, blank=True)
-    purchase_order = models.ForeignKey('PurchaseOrder', null=False, blank=False)
+    purchase_order = models.ForeignKey(
+        'PurchaseOrder',
+        null=False,
+        blank=False,
+        related_name='supplies'
+    )
     name = models.CharField(max_length=64, unique=True, null=True, blank=True)
     quantity = models.DecimalField(
         max_digits=8, decimal_places=2, null=True, blank=True,
@@ -148,23 +154,45 @@ class Supply(TimeStampedModel):
     receipt_date = models.DateTimeField(null=True, blank=True)
 
 
-class PurchaseOrder(TimeStampedModel):
 
-    class Meta:
-        verbose_name = 'Purchase Order'
-        verbose_name_plural = 'Purchase Orders'
+class PurchaseOrderManager(models.Manager):
+    def get_queryset(self):
+        queryset = super(PurchaseOrderManager, self).get_queryset()
+        queryset = queryset.annotate(
+            subtotal=Sum('supplies__cost'),
+            total=Sum('supplies__cost') + F('sales_tax') + F('shipping_cost'),
+            supplies_ordered=Count('supplies'),
+            supplies_received=Sum(
+                Case(
+                    When(supplies__receipt_date__isnull=False, then=1),
+                    default=0,
+                    output_field=IntegerField()
+                )
+            )
+        )
+        queryset = queryset.annotate(
+            percent_received=ExpressionWrapper(
+                Decimal('1.0')*F('supplies_received')/F('supplies_ordered'),
+                output_field=DecimalField()
+            )
+        )
+
+        return queryset
+
+class PurchaseOrder(TimeStampedModel):
 
     number = models.CharField(verbose_name='Purchase Order (PO) Number',
         max_length=64, unique=True, null=False, blank=False)
-    subtotal = models.DecimalField(
-        max_digits=8, decimal_places=2, default=Decimal(0.00),
-        validators=[MinValueValidator(Decimal('0.00'))])
     sales_tax = models.DecimalField(
         max_digits=8, decimal_places=2, default=Decimal(0.00),
         validators=[MinValueValidator(Decimal('0.00'))])
     shipping_cost = models.DecimalField(max_digits=8, decimal_places=2,
         null=False, blank=False, default=Decimal('0.00'))
-    total = models.DecimalField(max_digits=8, decimal_places=2, null=False,
-        blank=False, default=Decimal('0.00'))
     submitted = models.DateTimeField(default=timezone.now)
     warehouse = models.ForeignKey('Warehouse', null=False, blank=False)
+
+    objects = PurchaseOrderManager()
+
+    class Meta:
+        verbose_name = 'Purchase Order'
+        verbose_name_plural = 'Purchase Orders'
